7c8e5da17bb28af339dd2da03ac4c3e9b0c519a2
samael <samael.65535@gmail.com>
2017-02-16 10:47:10 +0800

    ex4.5

--- ch4/ex4.5/main.go
+++ ch4/ex4.5/main.go
@@ -1,26 +1,59 @@
 package main
-
+// 写一个函数在原地完成消除[]string中相邻重复的字符串的操作。
 import (
 	"fmt"
 )
 
 func main() {
 	arr := []string{
-		"abcd",
 		"aaaa",
+		"a",
+		"a",
 		"aaaa",
-		"abcd"}
-	fmt.Println(clearSpace(arr))
+		"a",
+	}
+
+	fmt.Println(clearDuplicate(arr))
+
+	arr1 := []string{
+		"a",
+		"a",
+	}
+	fmt.Println(clearDuplicate(arr1))
+
+
+	arr2 := []string{
+		"a",
+		"aaa",
+		"a",
+	}
+	fmt.Println(clearDuplicate(arr2))
+
+	arr3 := []string{
+		"a",
+		"a",
+	}
+	fmt.Println(clearDuplicate(arr3))
+
+
+	arr4 := []string{
+		"aaa",
+		"a",
+		"a",
+	}
+	fmt.Println(clearDuplicate(arr4))
+
 }
 
-func clearSpace(arr []string) []string {
+func clearDuplicate(arr []string) []string {
 	end := len(arr)
-	for i := 0; i < len(arr); i++ {
-		for j := i; j < len(arr); j++ {
-			if arr[i] == arr[j] {
-
+	for i := 0; i < len(arr) - 1; i++ {
+		if arr[i] == arr[i+1] {
+			for j := i; j < len(arr)- 1; j++ {
+				arr[j] = arr[j+1]
+				end--
 			}
 		}
 	}
-	return arr[0:end]
+	return arr[:end]
 }
