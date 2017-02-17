c5a4ea7ad3da3462154f47076e7d1cd0cd849bb9
samael65535 <samael@love.com>
2017-02-18 01:45:21 +0800

    ex5.1

---
+++ ch5/ex5.1/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	list, err := find(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	for _, res := range *list {
+		doc, err := html.Parse(bytes.NewReader(res))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+			os.Exit(1)
+		}
+		for _, link := range visit(nil, doc) {
+			fmt.Println(link)
+		}
+	}
+
+}
+
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+
+	links = visit(links, n.FirstChild)
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	links = visit(links, n.NextSibling)
+	// for c := n.FirstChild; c != nil; c = c.NextSibling {
+	// 	links = visit(links, c)
+	// }
+
+	return links
+}
+
+func find(urlList []string) (*[][]byte, error) {
+	list := [][]byte{{}}
+	for _, url := range urlList {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			return nil, err
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, b)
+	}
+	return &list, nil
+}
