31a95b357afb657c7c810b25630b3942380975b1
samael <samael.65535@gmail.com>
2017-02-20 09:35:23 +0800

    ex5.11

--- ch5/ex5.11/main.go
+++ ch5/ex5.11/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "os"
 
 // 重写topoSort函数，用map代替切片并移除对key的排序代码。验证结果的正确性（结果不唯一）。
 
@@ -24,40 +25,23 @@ var prereqs = map[string][]string{
 	"programming languages": {"data structures", "computer organization"},
 }
 
-var subsequent = map[string][]string{}
-
 func main() {
-	for i, course := range topoSort(prereqs) {
-		fmt.Printf("%d:\t%s\n", i+1, course)
+	slice, err := topoSort(prereqs)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
-	for k, v := range subsequent {
-		fmt.Println(k, v)
+	for i, course := range slice  {
+		fmt.Printf("%d:\t%s\n", i+1, course)
 	}
 }
 
-func topoSort(m map[string][]string) []string {
+func topoSort(m map[string][]string) ([]string, error){
 	var order []string
 	seen := make(map[string]bool)
-
-	var visitParent func(root string) error
-
-	visitParent = func(root, parent, current string) error {
-		if root == current {
-			return fmt.Errorf("err")
-		}
-		subsequent[root] = append(subsequent[root], current)
-		for _, c := range subsequent[parent] {
-			visitParent(root, current, subsequent[current])
-		}
-		return nil
+	for k := range m {
+		seen[k] = false
 	}
-
-	for k, v := range prereqs {
-		for _, c := range v {
-			visitParent(c, k, c)
-		}
-	}
-
 	var visitAll func(items map[string]bool)
 	visitAll = func(items map[string]bool) {
 		for k, v := range items {
@@ -80,8 +64,8 @@ func topoSort(m map[string][]string) []string {
 	}
 
 	visitAll(seen)
-	if len(order) != len(subsequent) {
-		return nil
+	if len(order) != len(seen) {
+		return nil, fmt.Errorf("cycle")
 	}
-	return order
+	return order, nil
 }
