62aef07e784ceccc96d41452731f2a3121a0200a
samael <samael.65535@gmail.com>
2017-02-20 09:42:26 +0800

    ex5.15

---
+++ ch5/ex5.15/main.go
@@ -0,0 +1,35 @@
+package main
+/*
+编写类似sum的可变参数函数max和min。
+考虑不传参时，max和min该如何处理，再编写至少接收1个参数的版本。
+*/
+import (
+	"fmt"
+)
+
+func min(vals ...int) int {
+	if (len(vals) == 0) {
+		panic()
+	}
+	return 0
+}
+
+func max(vals ...int) int {
+	if (len(vals) == 0) {
+		panic()
+	}
+	return 0
+}
+
+func sum (vals ...int) int {
+	total := 0
+	for _, val := range vals {
+		total += val
+	}
+	return total
+
+}
+func main() {
+	vals := []int{}
+	fmt.Println(sum(vals...))
+}
