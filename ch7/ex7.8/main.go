1ad7b01d9842310befa83d0c860a989d2e64c9c0
samael65535 <samael@love.com>
2017-02-23 08:19:05 +0800

    ex7.8

---
+++ ch7/ex7.8/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"io"
+)
+
+const debug = true
+
+func main() {
+	var buf *bytes.Buffer
+	if debug {
+		buf = new(bytes.Buffer) // enable collection of output
+	}
+	f(buf) // NOTE: subtly incorrect!
+	if debug {
+		// ...use buf...
+	}
+}
+
+// If out is non-nil, output will be written to it.
+func f(out io.Writer) {
+	// ...do something...
+	if out != nil {
+		out.Write([]byte("done!\n"))
+	}
+}
