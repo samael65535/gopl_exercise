53e1b8cdf846b9e6fa1ac47b1f14c8e4d4c36c2e
samael <samael.65535@gmail.com>
2017-02-24 16:21:54 +0800

    ex8.3

---
+++ ch8/ex8.3/main.go
@@ -0,0 +1,40 @@
+package main
+/*
+练习 8.3：
+修改netcat3的主goroutine代码，只关闭网络链接中写的部分，这样的话后台goroutine可以在标准输入被关闭后继续打印从reverb1服务器传回的数据。
+*/
+import (
+	"io"
+	"net"
+	"log"
+	"os"
+)
+func main() {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.CloseWrite()
+	<-done // wait for background goroutine to finish
+
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	_, err := io.Copy(dst, src)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
