96c4f90afc835bd735f171273aa471345f6af77b
samael <samael.65535@gmail.com>
2017-02-26 14:51:43 +0800

    ex8.7

--- ch8/ex8.7/main.go
+++ ch8/ex8.7/main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-练习 8.7：
+n练习 8.7：
 完成一个并发程序来创建一个线上网站的本地镜像，把该站点的所有可达的页面都抓取到本地硬盘。
 为了省事，我们这里可以只取出现在该域下的所有页面
 当然了，出现在页面里的链接你也需要进行一些处理，使其能够在你的镜像站点上进行跳转，而不是指向原始的链接。
@@ -19,14 +19,19 @@ import (
 	"../links"
 
 	"bytes"
+	"fmt"
 	"os"
 	"strings"
 )
 
+const BasePath = "./output/"
+var maxDepth int
+var maxCrawler int
+var pipe chan int
 // breadthFirst calls f for each item in the worklist.
 // Any items returned by f are added to the worklist.
 // f is called at most once for each item.
-func breadthFirst(f func(item string, hosts map[string]bool, depth int) []string, worklist [][]string) {
+func breadthFirst(f func(item string, hosts map[string]bool, depth int)) {
 	seen := make(map[string]bool)
 	hosts := make(map[string]bool)
 	for _, k := range worklist[0] {
@@ -34,75 +39,79 @@ func breadthFirst(f func(item string, hosts map[string]bool, depth int) []string
 		hosts[s.Host] = true
 		_ = os.Mkdir(BasePath+s.Host, 0755)
 	}
-
+	maxDepth = 4
+	maxCrawler = 20
+	pipe = make(chan int, maxCrawler)
+	for i := 0 ; i < maxCrawler; i++ {
+		pipe <- 1
+	}
 	for len(worklist) > 0 {
 		for depth, items := range worklist {
-			worklist = append(worklist, []string{})
-			for _, item := range items {
-				if !seen[item] {
-					seen[item] = true
-					list := f(item, hosts, depth)
-					worklist[depth+1] = append(worklist[depth+1], list...)
-
+			if depth < maxDepth {
+				worklist = append(worklist, []string{})
+				for _, item := range items {
+					if !seen[item] {
+						seen[item] = true
+						<-pipe
+						go f(item, hosts, depth)
+					}
 				}
 			}
 		}
 	}
 }
 
-const BasePath = "./output/"
+func crawl(u string, hosts map[string]bool, depth int,) {
 
-func crawl(u string, hosts map[string]bool, depth int) []string {
 	currentURL, e := url.Parse(u)
+		defer func() {
+		pipe <- 1
+	}()
 	if !hosts[currentURL.Host] {
-		return nil
+		return
 	}
-	list, err := links.Extract(u)
-
-	if err != nil {
-		log.Print(err)
-		return list
-	}
-
-	if hosts[currentURL.Host] && e == nil && currentURL.Path != "" {
-		resp, err := http.Get(u)
+	if hosts[currentURL.Host] && e == nil {
+		list, err := links.Extract(u)
 		if err != nil {
-			resp.Body.Close()
-			return list
+			log.Print(err)
+			return
+		}
+		resp, err := http.Get(u)
+		defer resp.Body.Close()
+		if err != nil {
+			defer resp.Body.Close()
+			return
 		}
 		body, e := ioutil.ReadAll(resp.Body)
 		if e != nil {
-			resp.Body.Close()
 			log.Print(err)
-			return list
+			return
 		}
 
 		node, _ := html.Parse(bytes.NewReader(body))
-		forEachNode(node, replaceLink, nil, depth)
+		forEachNode(node, replaceLink, nil, depth, currentURL)
 		e = writeFile(node, currentURL, resp)
 
 		if e != nil {
-			resp.Body.Close()
-			log.Print(err)
-			return list
+			log.Print(e)
+			return
 		}
-		//fmt.Printf("Crawling...\t%s\t%s\t%s\n", currentURL.Path, filename, u)
-		resp.Body.Close()
-		return list
+		fmt.Printf("Crawling...\t%s\t%d\n", currentURL.Path, depth)
+		worklist[depth+1] = append(worklist[depth+1], list...)
+		return
 	}
-	return list
+
 }
 
 func writeFile(node *html.Node, currentURL *url.URL, resp *http.Response) error {
-	filename := BasePath + currentURL.Host + currentURL.Path
+	filename := BasePath + currentURL.Host + currentURL.Path + "/index.html"
 	e := os.MkdirAll(path.Dir(filename), 0755)
-	if filename == "./"+path.Dir(filename)+"/" {
-		filename = filename + "/index.html"
-	}
-	// os
+	fmt.Println(filename)
+	_ = fmt.Errorf
 	if e != nil {
 		resp.Body.Close()
 		log.Print(e)
+		return e
 	}
 
 	b := &bytes.Buffer{}
@@ -114,6 +123,7 @@ func writeFile(node *html.Node, currentURL *url.URL, resp *http.Response) error 
 	if e != nil {
 		return e
 	}
+
 	return nil
 }
 
@@ -121,31 +131,35 @@ func writeFile(node *html.Node, currentURL *url.URL, resp *http.Response) error 
 // pre和post都是可选的。
 // 遍历孩子结点之前,pre被调用
 // 遍历孩子结点之后，post被调用
-func replaceLink(n *html.Node, depth int) {
+func replaceLink(n *html.Node, depth int, currentURL *url.URL) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for i, a := range n.Attr {
 			if a.Key == "href" {
-				strings.Repeat("../")
-				n.Attr[i].Val = n.Attr[i].Val
+				link, err := url.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				n.Attr[i].Val = "./" + strings.Repeat("../", depth) + link.Path + "/index.html"
 			}
 		}
 	}
 }
-func forEachNode(n *html.Node, pre, post func(n *html.Node, d int), depth int) {
+func forEachNode(n *html.Node, pre, post func(n *html.Node, d int, curUrl *url.URL), depth int, curUrl *url.URL) {
 	if pre != nil {
-		pre(n, depth)
+		pre(n, depth, curUrl)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		forEachNode(c, pre, post, depth)
+		forEachNode(c, pre, post, depth, curUrl)
 	}
 
 	if post != nil {
-		post(n, depth)
+		post(n, depth, curUrl)
 	}
 }
 
+var worklist [][]string
 func main() {
 	_ = url.Parse
-	worklist := [][]string{[]string{"http://www.baidu.com"}}
-	breadthFirst(crawl, worklist)
+	worklist = [][]string{[]string{"http://www.ituring.com.cn"}}
+	breadthFirst(crawl)
 }
